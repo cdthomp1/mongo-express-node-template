cd77bc5e17b1bbddb43f7a66a809415cd9c84388
MeerHamza1421 <hamzababar37@gmail.com>
2021-10-08 18:00:36 -0700

    📦 NEW: Add Project Intializer

---
+++ functions/init.js
@@ -0,0 +1,32 @@
+const exec = require('node-async-exec');
+const ora = require('ora');
+
+module.exports = async (projectName) => {
+    const spinner = ora('creating project folder...').start();
+
+    try {
+        await exec({
+            path: process.cwd(),
+            cmd: `mkdir ${projectName}`
+        })
+        spinner.succeed(`created folder ${projectName}`)
+        await gitInitializer(projectName);
+
+    }
+    catch (err) {
+        return spinner.fail(`fail to create ${projectName}`);
+    }
+}
+
+const gitInitializer = async (projectName) => {
+    try {
+        const spinner = ora('initializing git...').start();
+        await exec({
+            path: `${process.cwd()}/${projectName}`,
+            cmd: `git init`
+        })
+        spinner.succeed('initialized empty git repository')
+    } catch (err) {
+        return spinner.fail(`fail to initialized git`);
+    }
+}
