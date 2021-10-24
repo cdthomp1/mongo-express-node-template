20dd05c48a7f742f5277c9a641fc5f53bf72db78
HaiderSultanArc <haidersultanarc@gmail.com>
2021-10-24 17:51:44 +0500

    Modified Script for using templates and Updated Readme

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-#!/usr/bin/env node
-
+// #!/usr/bin/env node
 const header = require("./utils/header");
 const cli = require('./utils/cli');
 
 (() => {
     header();
-    const projectName = process.argv.slice(2);
-    cli(projectName[0]);
+    
+    if (process.argv.length == 3) {
+        const project = process.argv.slice(2);
+        const projectName = project[0];
+        
+        cli(projectName);
+    }
+    else if (process.argv.length == 4) {
+        const project = process.argv.slice(2);
+        const projectName = project[0];
+        const projectTemplate = project[1];
+        
+        cli(projectName, projectTemplate);
+    }
 })();
--- utils/cli.js
+++ utils/cli.js
@@ -3,11 +3,19 @@ const clientCodeGenerator = require('../functions/client-code-generator');
 const serverCodeGenerator = require('../functions/server-code-generator');
 const ora = require('ora');
 
-module.exports = async (projectName) => {
-
+module.exports = async (projectName, projectTemplate) => {
+    projectTemplate = projectTemplate || "";
+    
+    if (projectTemplate.length) {
+        console.log(`Creating Project ${projectName} using template ${projectTemplate}`);
+    }
+    else {
+        console.log(`Creating Project ${projectName}`);
+    }
+    
     try {
         await init(projectName);
-        await clientCodeGenerator();
+        await clientCodeGenerator(projectTemplate);
         await serverCodeGenerator();
         ora().start().succeed("created mern stack app")
     }
